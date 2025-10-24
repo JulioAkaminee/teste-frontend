854235836bd6916797c80382336b9534eb8d828b
JulioAkaminee <juliooakamine@gmail.com>
2025-10-23 23:27:15 -0300

    instalando e configurando tailwind e react router

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,13 @@
+    
+
+function Home() {
+  return (
+    <div>
+       <h1 className="text-2xl font-bold underline text-blue-500">
+      Olá, Tailwind CSS com React e TypeScript!
+    </h1>
+    </div>
+  );
+}
+
+export default Home;
