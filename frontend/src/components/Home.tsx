9597dc55cbf8babe4a398474970243ec4be8a8f9
JulioAkaminee <juliooakamine@gmail.com>
2025-10-27 13:54:48 -0300

    Construindo a pagina home com a barra de pesquisa

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,12 +2,13 @@
 
 function Home() {
   return (
-    <div>
-       <h1 className="text-2xl font-bold underline text-blue-500">
-      Olá, Tailwind CSS com React e TypeScript!
-    </h1>
+    <div className="flex justify-center items-center flex-col bg-background" >
+       <h1 className="font-bold flex justify-center text-white m-10 text-4xl">Find People</h1>
+
+       <input type="search" name="searchUsers" id="SearchUsers" placeholder="Search people..." className="bg-transparent w-4/12 rounded-xl p-3 mt-4 border-2 outline-none text-white"  />
+             <hr className="border-white w-full my-6" />  
     </div>
-  );
+  );    
 }
 
 export default Home;
