43fc3a793fb73477c48e45b196a2c672c3be9ea2
JulioAkaminee <juliooakamine@gmail.com>
2025-10-27 22:21:46 -0300

    update deploy settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import UserDetails from './pages/UserDetails';
 
 function App() {
   return (
-    <BrowserRouter> 
+    <BrowserRouter basename={process.env.PUBLIC_URL}> 
     <Routes>
       <Route path="/" element={<Home/>} />
         <Route path="/user/:uuid" element={<UserDetails />} />
